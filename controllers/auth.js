7056bcaa57af6c6070bd29ba4cd250902c8cd76e
Alish Bista <bistace321@gmail.com>
2023-09-28 20:20:12 +0545

    feat: Created Password change feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,6 +57,28 @@ const handleEmailVerification = async (req,res) => {
     res.status(StatusCodes.OK).json({msg: "Email Verification True"})
 }
 
+// handle Change password
+const handleChangePassword = async (req,res) =>{
+    const id = req.user.userId
+    const {currentPassword,newPassword,verificationPassword} = req.body
+    if (!currentPassword || !newPassword || !verificationPassword ){
+        throw new NotFoundError("Missing currentPassword or newPassword or verificationPassword")
+    }
+    if (newPassword !== verificationPassword) {
+        throw new NotFoundError("newPassword and verificationPassword do not match")
+    }
+    const user = await User.findOne({_id:id})
+    console.log(user)
+    const isPasswordCorrect = await user.comparePassword(currentPassword)
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError("Invalid Current Password")
+    }
+    user.password = newPassword;
+    await user.save()
+    res.status(StatusCodes.CREATED).json({msg: "Password Changed Successfully"})
+}
+
+
 // Handle token Refresh
 const handleTokenRefresh = (req,res) => {
    res.status(StatusCodes.OK).json({
@@ -103,5 +125,6 @@ module.exports = {
     handlePasswordResetRequest,
     handlePasswordReset,
     handleGetProfile,
-    handleResendEmailVerification
+    handleResendEmailVerification,
+    handleChangePassword
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
     This is the unauthenticated Routes.
 */
 const express = require('express');
-
+const authMiddleware = require('../middlewares/auth');
 const authRoutes = express.Router()
 const {
     handleRegister,
@@ -10,15 +10,21 @@ const {
     handlePasswordReset,
     handlePasswordResetRequest,
     handleEmailVerification,
-    handleResendEmailVerification
+    handleResendEmailVerification,
+    handleChangePassword
 
 
 }  = require('../controllers/auth')
 
+// Unauthorized Routes
 authRoutes.post("/login",handleLogin)
 authRoutes.post('/register',handleRegister)
 authRoutes.get("/verify/:token",handleEmailVerification)
 authRoutes.post('/reset-password/:token',handlePasswordReset)
 authRoutes.get('/reset-password-request',handlePasswordResetRequest)
+
+// Authorized Routes
+authRoutes.post("/change-password",authMiddleware,handleChangePassword)
+
 
 module.exports = authRoutes
