03bc354598acab2d07ec4f8b14efd74b8ec187e8
Alish Bista <bistace321@gmail.com>
2023-09-30 06:22:44 +0545

    feat: Create new roles successful

--- controllers/role.js
+++ controllers/role.js
@@ -13,11 +13,13 @@ const getAllRoles = async (req,res) => {
 const getRole = async (req,res) => {
     const roleID = req.params.id
     const roles = await Role.findOne({_id:roleID})
-    return res.status(200).json({message:`Get Role Successful`,data:roles})
+    return res.status(StatusCodes.OK).json({message:`Get Role Successful`,data:roles})
 }
 // Create A New Role
-const createRole = (req,res) => {
-    return res.status(200).json({message:"Create Role ID"})
+const createRole = async (req,res) => {
+    const {name,description} = req.body;
+    const role = await Role.create({name,description})
+    return res.status(200).json({message:"Added New Role", data:role})
 }
 
 // Update Existing Roles
--- models/Role.js
+++ models/Role.js
@@ -3,18 +3,20 @@ const mongoose = require('mongoose');
 const roleSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    unique: true,
+    required: [true, "name is required"],
+    unique: [true,"Already Present"],
     trim: true,
+    minlength: 3,
+    maxlength: 32
   },
   description: {
     type: String,
-    required: true,
+    required: [true, "description is required"],
   },
   permissions: [
     {
       type: String,
-      required: true,
+     
     },
   ],
 });
